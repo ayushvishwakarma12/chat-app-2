5c1443fa4393f4f0ec2f3ad562ae6e43b87cb5be
Ayush Vishwakarma <ayushvishwakarma114@gmail.com>
2024-03-08 18:34:11 +0530

    First commit

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkelton from "../skeletons/MessageSkelton";
+import Message from "./Message";
+import useListenMessages from "../../hooks/useListenMessages";
+
+const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  useListenMessages();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behaviour: "smooth" });
+    }, 500);
+  }, [messages]);
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkelton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
+    </div>
+  );
+};
+
+export default Messages;
---
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,38 @@
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
+  return (
+    <div className="flex">
+      <div className="form-control">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "male" ? "selected" : ""
+          }`}
+        >
+          <span>Male</span>
+          <input
+            type="checkbox"
+            className="checkbox border-slate-900"
+            checked={selectedGender === "male"}
+            onChange={(e) => onCheckboxChange("male")}
+          />
+        </label>
+      </div>
+      <div className="form-control">
+        <label
+          className={`label gap-2 cursor-pointer  ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
+          <span>Female</span>
+          <input
+            type="checkbox"
+            className="checkbox border-slate-900"
+            checked={selectedGender === "female"}
+            onChange={(e) => onCheckboxChange("female")}
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default GenderCheckbox;
